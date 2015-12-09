1685b92e40a804ab4c3890fb278f35eab119534d
djeemie2000 <peter.jamee@telenet.be>
2015-12-09 11:21:13 +0100

    Serial (midi) in: working example of buffered serial read.

--- SerialMidiInHelloWorld/main.cpp
+++ SerialMidiInHelloWorld/main.cpp
@@ -1,7 +1,31 @@
 #include "mbed.h"
 
 //Serial pc6(PA_11, PA_12);//serial 6
+
+const int RecieveBufferSize = 32;
+char g_RecieveBuffer[RecieveBufferSize];
+int g_RecieveCounterBuffer[RecieveBufferSize];
+int g_RecieveCounter;
+
 Serial pc6(D8, D2);//serial 1??
+
+void OnRecieve()
+{
+    int Index = g_RecieveCounter%RecieveBufferSize;
+    g_RecieveBuffer[Index] = pc6.getc();
+    g_RecieveCounterBuffer[Index] = g_RecieveCounter;
+    ++g_RecieveCounter;
+}
+
+void PrintRecieveBuffer()
+{
+    printf("Buffer: \r\n");
+    for(int idx = 0; idx<RecieveBufferSize; ++idx)
+    {
+        printf("%d : 0x%x \r\n", g_RecieveCounterBuffer[idx], g_RecieveBuffer[idx]);
+    }
+    printf("\r\n");
+}
 
 int main()
 {
@@ -14,28 +38,18 @@ int main()
     int Baud = 9600;
     //int Baud = 31250;/midi baudrate
     pc6.baud(Baud);
-
     printf("Baud rate = %d \r\n", Baud);
 
-    int Counter = 0;
+    g_RecieveCounter = 0;
+    pc6.attach(&OnRecieve);
+
     while(1)
     {
-        const int ReadBufferSize = 3;
-        char ReadBuffer[ReadBufferSize];
-
-        uint32_t idx = 0;
-        while(idx < ReadBufferSize)
+        int Cntr = g_RecieveCounter;
+        wait(0.2f);
+        if(Cntr!=g_RecieveCounter)
         {
-            while(!pc6.readable()); // busy waiting for data available
-            ReadBuffer[idx++] = pc6.getc();
+            PrintRecieveBuffer();
         }
-
-        for(int idx = 0; idx<ReadBufferSize; ++idx)
-        {
-            printf("0x%x ", ReadBuffer[idx]);
-        }
-        printf("\r\n");
-
-        ++Counter;
     }
 }
