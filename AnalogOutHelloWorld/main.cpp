5cebfd3f7ee001ca0985bcbc814f72be8ff9c076
Jamée, Peter <Peter.Jamee@flir.com>
2016-02-23 20:05:03 +0100

    AnalogOut HelloWorld: saw/pulse wave out.

--- AnalogOutHelloWorld/main.cpp
+++ AnalogOutHelloWorld/main.cpp
@@ -9,8 +9,58 @@ Serial pc(USBTX, USBRX);
 
 AnalogOut myAnOut(A2);
 
+void SawOut(float Frequency, float PulseWidth)
+{
+    pc.printf("SawOut\r\n");
+
+    Timer t;
+    t.start();
+
+    float SamplingFrequency = 40000.0f;
+    int PeriodMicroSeconds = 1000000/SamplingFrequency;
+
+    float PhaseIncr = Frequency/SamplingFrequency;
+
+    pc.printf("Fs=%f F=%f Incr=%f\r\n", SamplingFrequency, Frequency, PhaseIncr);
+
+    float Phase = 0;
+    for(int Repeat = 0; Repeat<SamplingFrequency; ++Repeat)
+    {
+        //float Out = Phase<PulseWidth?1:0;
+        float Out = Phase;
+        myAnOut.write(Out);
+
+        Phase += PhaseIncr;
+        if(1.0f<Phase)
+        {
+            Phase -= 1;
+        }
+
+        wait_us(PeriodMicroSeconds);
+    }
+    myAnOut.write(0.5f);
+
+    t.stop();
+    pc.printf("Elapsed %d uSec \r\n", t.read_us());
+}
+
 int main()
 {
+    pc.printf("AnalogOut HelloWorld!");
+
+    float PulseWidth = 0.1f;
+    float Frequency = 220.0f;
+    while(true)
+    {
+        SawOut(Frequency, PulseWidth);
+        PulseWidth+=0.1f;
+        if(1<PulseWidth)
+        {
+            PulseWidth = 0.1f;
+        }
+        //wait(1.0f);
+    }
+
   int i = 0;
   while(true)
   {
